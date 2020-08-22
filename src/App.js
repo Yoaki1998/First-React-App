48fb8cad3132cde6d56d37d83ffb31eea058e9a1
Yoaki Niscoise <yoakiniscoise@gmail.com>
2020-08-22 02:36:22 +0200

    fix comflict yarn and npm

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const team = {
+  membre1: {
+    nom: "Yoaki",
+    age: 22,
+  },
+  membre2: {
+    nom: "Random",
+    age: 25,
+  },
+};
+
+class App extends Component {
+  state = {
+    team,
+  };
+  render() {
+    return (
+      <div className='App'>
+        <h1> {this.state.team.membre1.nom} </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
