6370e641a8eb84232aca8e83562ada169ae775ef
Yoaki Niscoise <yoakiniscoise@gmail.com>
2020-08-24 14:02:17 +0200

    Auto update input

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from "react"
+import "./App.css"
 
 const team = {
   membre1: {
     nom: "Yoaki",
-    age: 22,
+    age: 22
   },
   membre2: {
     nom: "Random",
-    age: 25,
-  },
-};
-
-class App extends Component {
-  state = {
-    team,
-  };
-  render() {
-    return (
-      <div className='App'>
-        <h1> {this.state.team.membre1.nom} </h1>
-      </div>
-    );
+    age: 25
   }
 }
 
-export default App;
+class App extends Component {
+  state = {
+    team
+  }
+  handleChange = (event) => {
+    const team = { ...this.state.team }
+    const nom = event.target.value
+    console.log(nom)
+    team.membre1.nom = nom
+    this.setState({ team })
+  }
+  render() {
+    return (
+      <div className='App'>
+        <input
+          type='text'
+          value={this.state.team.membre1.nom}
+          onChange={this.handleChange}
+        />
+        <h1> {this.state.team.membre1.nom} </h1>
+        <h1> {this.state.team.membre2.nom} </h1>
+      </div>
+    )
+  }
+}
+
+export default App
